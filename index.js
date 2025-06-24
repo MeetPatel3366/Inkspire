6411d924b9e292f247c07a313348f41f3bc7bc60
Meet Patel <meet3366patel@gmail.com>
2025-06-24 18:38:36 +0530

    create home page, head section & navbar

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.get('/',(req,res)=>{
+    res.render("home")
+})
+
 app.listen(PORT, () => {
   console.log("server started at port : ", PORT);
 });
